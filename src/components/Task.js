fffb1105df84f465740ed5fc4425fde1daa22ab4
ketan mehta <meetketanmehta@gmail.com>
2024-07-21 18:23:03 +0530

    remove dist

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-const Task = ({task}) =>{
+const Task = ({ task, onToggle }) => {
   return (
     <div key={task.id} className="box-shadow taskbox">
-      
-      <div>Task: {task.task}</div>
-      <div>Date: {task.date}</div>
-      <div>Done: {task.done}</div>
+      <div>
+        <input type="checkbox" checked={task?.done && task.done === 'true'} onChange={onToggle} />
+      </div>
+      <div>
+        <div>Task: {task.task}</div>
+        <div>Date: {task.date}</div>
+        <div>Done: {task.done}</div>
+      </div>
     </div>
   );
-}
+};
 export default Task;
--- src/components/Today.js
+++ src/components/Today.js
@@ -5,13 +5,25 @@ import Task from './Task';
 const Today = () => {
   const [tasks, setTasks] = useState([]);
 
+  const handleToggle = (id) => {
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, done: task.done === 'true' ? 'false' : 'true' } : task)));
+  };
+
   useEffect(() => {
     // Convert the object to an array for easier mapping
     const tasksArray = Object.entries(allTasks).map(([key, value]) => ({ id: key, ...value }));
     setTasks(tasksArray);
   }, []);
 
-  return <>{tasks.length > 0 ? tasks.map((task) => <Task task={task} id={task.id} />) : <p>No tasks found.</p>}</>;
+  return (
+    <>
+      {tasks.length > 0 ? (
+        tasks.map((task) => <Task task={task} onToggle={() => handleToggle(task.id)} />)
+      ) : (
+        <p>No tasks found.</p>
+      )}
+    </>
+  );
 };
 
 export default Today;
