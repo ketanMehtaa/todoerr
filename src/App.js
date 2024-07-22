2c7c92c903a1bb7ba602fe201f943ffa905d5bad
ketan mehta <meetketanmehta@gmail.com>
2024-07-22 16:36:12 +0530

    redirect to login if no user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
 import Today from './components/Today';
 
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../src/firebaseConfig';
 
 const AppLayout = () => {
+  const [user, loadingAuth] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+
+    if(!user){
+      navigate('/login'); // Redirect to login if no user is signed in
+    }
+  },[user])
   return (
     <>
       <Navbar />
